43ec6947e49d554ec6f96db5334ce1190196f65f
darwageddon <canberk.ertrn@hotmail.com>
2022-04-10 18:51:09 +0300

    Latest Version

---
+++ Assets/PrintToConsole.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintToConsole : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hey there! Wait a minute! I got no doors or windows + nobody's driving me. Who the hell designed me like this? Rrrrrrr...");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
